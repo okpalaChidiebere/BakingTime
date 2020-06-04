3618b35902005b2ffed8b857917167ada85cea9c
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-06-04 11:27:41 -0600

    Broken: Implementing RetroFit
    Problem loading all the response of Baking food.

--- app/src/main/java/com/example/android/baking/MainActivity.java
+++ app/src/main/java/com/example/android/baking/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.android.baking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.android.baking.Model.BakingFood;
+import com.example.android.baking.Model.Ingredients;
+import com.example.android.baking.Utils.ApiClient;
+import com.example.android.baking.Utils.ApiInterface;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+        Call<List<BakingFood>> call = apiInterface.getBakingFoods();
+        call.enqueue(new Callback<List<BakingFood>>() {
+            @Override
+            public void onResponse(Call<List<BakingFood>> call, Response<List<BakingFood>> response) {
+                Log.e(TAG, "onResponse "+ response.body());
+
+                /*Log.v("RESPONSE_CALLED", "ON_RESPONSE_CALLED");
+                String didItWork = String.valueOf(response.isSuccessful());
+                Log.v("SUCCESS?", didItWork);
+                Log.v("RESPONSE_CODE", String.valueOf(response.code()));
+                BakingFood food = response.body().;
+                Log.v("RESPONSE_BODY", "response:" + food);
+                String total = response.body().getName().toString();
+                Log.v("Total", total);
+                List<Ingredients> ingredientsResults = response.body().get(1).setIngredients();
+                for (Ingredients ingredients : ingredientsResults) {
+                    Log.v("PHOTO_URL:", ingredients.getIngredient()
+                    );
+                }*/
+            }
+
+            @Override
+            public void onFailure(Call<List<BakingFood>> call, Throwable t) {
+                Log.e(TAG, "onFailure "+ t.getLocalizedMessage());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/android/baking/Model/BakingFood.java
@@ -0,0 +1,83 @@
+package com.example.android.baking.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BakingFood {
+
+    private int id;
+    private String name;
+    private List<Ingredients> ingredients = new ArrayList<Ingredients>();
+    private List<Steps> steps = new ArrayList<Steps>();
+    private int servings;
+    private String image;
+
+    /*public BakingFood(int id, String name, List<Ingredients> ingredients, List<Steps> steps, int servings, String image) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }*/
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Steps> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Steps> steps) {
+        this.steps = steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "BakingFood{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ingredients=" + ingredients +
+                ", steps=" + steps +
+                ", servings=" + servings +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
