6ccf00fd3a1c9a76b1c3d2a1f4c4afd468f13eb6
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-06-07 07:32:31 -0600

    Implemented PlayerView and populating recipe step description page
    Implemented PlayerView with fullscreen mode and also fullscreen mode for LandScape mode programatically.
    
    I had to hide the PlayerView when there is no video from API on a recipe step

--- app/src/main/java/com/example/android/baking/RecipeStepDescription.java
+++ app/src/main/java/com/example/android/baking/RecipeStepDescription.java
@@ -1,15 +1,41 @@
 package com.example.android.baking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.example.android.baking.Model.BakingFood;
 import com.example.android.baking.Model.Steps;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlaybackControlView;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.List;
 
@@ -20,20 +46,41 @@ public class RecipeStepDescription extends AppCompatActivity {
 
     private Steps mSteps;
 
+
+    /*exo player used variables*/
+    private final String STATE_RESUME_WINDOW = "resumeWindow";
+    private final String STATE_RESUME_POSITION = "resumePosition";
+    private final String STATE_PLAYER_FULLSCREEN = "playerFullscreen";
+
+    private PlayerView mPlayerView;
+    private MediaSource mVideoSource;
+    private boolean mExoPlayerFullscreen = false;
+    private FrameLayout mFullScreenButton;
+    private ImageView mFullScreenIcon;
+    private Dialog mFullScreenDialog;
+
+    private int mResumeWindow;
+    private long mResumePosition;
+    private String mMediaURL;
+    private FrameLayout mediaFrame;
+    /*End exo player used variables*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_step_description);
 
-        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back Button
+        TextView recipeDescription;
 
-        //If the phone is in landscape mode, we hide the status and action bar so that the user can
-        // watch the video properly
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        recipeDescription = (TextView) findViewById(R.id.tv_recipe_step_instruction);
+
+        if (savedInstanceState != null) {
+            mResumeWindow = savedInstanceState.getInt(STATE_RESUME_WINDOW);
+            mResumePosition = savedInstanceState.getLong(STATE_RESUME_POSITION);
+            mExoPlayerFullscreen = savedInstanceState.getBoolean(STATE_PLAYER_FULLSCREEN);
         }
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true); //back Button
 
         Intent intentThatStartedThisActivity = getIntent();
 
@@ -42,7 +89,174 @@ public class RecipeStepDescription extends AppCompatActivity {
             if (intentThatStartedThisActivity.hasExtra(EXTRA_RECIPE_STEPS)) {
                 mSteps = (Steps) intentThatStartedThisActivity.getSerializableExtra(EXTRA_RECIPE_STEPS);
                 setTitle(mSteps.getShortDescription()); //page label
+                mMediaURL = mSteps.getVideoURL();
+                recipeDescription.setText(mSteps.getDescription());
             }
+
         }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        outState.putInt(STATE_RESUME_WINDOW, mResumeWindow);
+        outState.putLong(STATE_RESUME_POSITION, mResumePosition);
+        outState.putBoolean(STATE_PLAYER_FULLSCREEN, mExoPlayerFullscreen);
+
+        super.onSaveInstanceState(outState);
+    }
+
+
+    private void initFullscreenDialog() {
+
+        mFullScreenDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen) {
+            public void onBackPressed() {
+                if (mExoPlayerFullscreen)
+                    closeFullscreenDialog();
+                super.onBackPressed();
+            }
+        };
+    }
+
+
+    private void openFullscreenDialog() {
+
+        ((ViewGroup) mPlayerView.getParent()).removeView(mPlayerView);
+        mFullScreenDialog.addContentView(mPlayerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(RecipeStepDescription.this, R.drawable.ic_fullscreen_skrink));
+        mExoPlayerFullscreen = true;
+        mFullScreenDialog.show();
+    }
+
+
+    private void closeFullscreenDialog() {
+
+        /*  Locks the device window in portrait mode. */
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        /* Allows user to freely use portrait or landscape mode. */
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+
+        ((ViewGroup) mPlayerView.getParent()).removeView(mPlayerView);
+        ((FrameLayout) findViewById(R.id.main_media_frame)).addView(mPlayerView);
+        mExoPlayerFullscreen = false;
+        mFullScreenDialog.dismiss();
+        mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(RecipeStepDescription.this, R.drawable.ic_fullscreen_expand));
+    }
+
+
+    private void initFullscreenButton() {
+
+        PlaybackControlView controlView = mPlayerView.findViewById(R.id.exo_controller);
+        mFullScreenIcon = controlView.findViewById(R.id.exo_fullscreen_icon);
+        mFullScreenButton = controlView.findViewById(R.id.exo_fullscreen_button);
+        mFullScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mExoPlayerFullscreen)
+                    openFullscreenDialog();
+                else
+                    closeFullscreenDialog();
+            }
+        });
+    }
+
+
+    private void initExoPlayer() {
+
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        LoadControl loadControl = new DefaultLoadControl();
+        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this), trackSelector, loadControl);
+        mPlayerView.setPlayer(player);
+
+        boolean haveResumePosition = mResumeWindow != C.INDEX_UNSET;
+
+        if (haveResumePosition) {
+            player.seekTo(mResumeWindow, mResumePosition);
+        }
+
+        player.setPlayWhenReady(true);
+        player.prepare(mVideoSource, !haveResumePosition, false);
+    }
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        mPlayerView.getPlayer().stop();
+        mPlayerView.getPlayer().release();
+        mPlayerView = null;
+    }
+
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        if (mPlayerView == null) {
+
+            mPlayerView = (PlayerView) findViewById(R.id.exoplayer);
+            mediaFrame = (FrameLayout) findViewById(R.id.main_media_frame);
+
+            //Hide the Player View when there is no video url from API
+            if(!mSteps.getVideoURL().equals("")){
+                mediaFrame.setVisibility(View.VISIBLE);
+            }
+
+            initFullscreenDialog();
+            initFullscreenButton();
+
+            String streamUrl = mMediaURL; //eg https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4
+            String userAgent = Util.getUserAgent(RecipeStepDescription.this, getApplicationContext().getApplicationInfo().packageName);
+            DefaultHttpDataSourceFactory httpDataSourceFactory = new DefaultHttpDataSourceFactory(userAgent, null, DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS, DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS, true);
+            DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(RecipeStepDescription.this, null, httpDataSourceFactory);
+            Uri daUri = Uri.parse(streamUrl);
+
+            mVideoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(daUri, null, null);
+        }
+
+        initExoPlayer();
+
+        if (mExoPlayerFullscreen) {
+            ((ViewGroup) mPlayerView.getParent()).removeView(mPlayerView);
+            mFullScreenDialog.addContentView(mPlayerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(RecipeStepDescription.this, R.drawable.ic_fullscreen_skrink));
+            mFullScreenDialog.show();
+        }
+
+        //If the phone is in landscape mode, we open the video in fullScreen
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            openFullscreenDialog();
+        }else{
+            //at this point, the phone is rotated back to portrait mode, so we exit fullscreen mode
+            closeFullscreenDialog();
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+
+        if (mPlayerView != null && mPlayerView.getPlayer() != null) {
+            mResumeWindow = mPlayerView.getPlayer().getCurrentWindowIndex();
+            mResumePosition = Math.max(0, mPlayerView.getPlayer().getContentPosition());
+
+            mPlayerView.getPlayer().release();
+        }
+
+        if (mFullScreenDialog != null)
+            mFullScreenDialog.dismiss();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
     }
 }
